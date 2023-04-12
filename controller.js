e7b5e65d1ec92a0ee46f7dcde01bd8946905b5cc
@hrahmat950 <hrahmat950@gmail.com>
2023-04-12 21:16:00 +0700

    test

--- controller.js
+++ controller.js
@@ -1,45 +1,45 @@
 const foodModel = require('./models');
 
-const getFoods = (req, res) => {
-  const foods = foodModel.getFoods();
-  res.json(foods);
+const getFoods = async (req, res) => {
+const foods = await foodModel.getFoods();
+res.json(foods);
 };
 
-const getFoodById = (req, res) => {
-  const foodId = req.params.foodId;
-  const food = foodModel.getFoodById(foodId);
-  if (food) {
-    res.json(food);
-  } else {
-    res.status(404).json({ message: "Food not found" });
-  }
+const getFoodById = async (req, res) => {
+const foodId = req.params.foodId;
+const food = await foodModel.getFoodById(foodId);
+if (food) {
+res.json(food);
+} else {
+res.status(404).json({ message: "Food not found" });
+}
 };
 
-const addFood = (req, res) => {
-  const newFood = req.body;
-  const addedFood = foodModel.addFood(newFood);
-  res.json(addedFood);
+const addFood = async (req, res) => {
+const newFood = req.body;
+const addedFood = await foodModel.addFood(newFood);
+res.json(addedFood);
 };
 
-const updateFood = (req, res) => {
-  const foodId = req.params.foodId;
-  const updatedFood = req.body;
-  const result = foodModel.updateFood(foodId, updatedFood);
-  if (result) {
-    res.json(result);
-  } else {
-    res.status(404).json({ message: "Food not found" });
-  }
+const updateFood = async (req, res) => {
+const foodId = req.params.foodId;
+const updatedFood = req.body;
+const result = await foodModel.updateFood(foodId, updatedFood);
+if (result) {
+res.json(result);
+} else {
+res.status(404).json({ message: "Food not found" });
+}
 };
 
-const deleteFood = (req, res) => {
-  const foodId = req.params.foodId;
-  const result = foodModel.deleteFood(foodId);
-  if (result) {
-    res.json(result);
-  } else {
-    res.status(404).json({ message: "Food not found" });
-  }
+const deleteFood = async (req, res) => {
+const foodId = req.params.foodId;
+const result = await foodModel.deleteFood(foodId);
+if (result) {
+res.json(result);
+} else {
+res.status(404).json({ message: "Food not found" });
+}
 };
 
 module.exports = { getFoods, getFoodById, addFood, updateFood, deleteFood };
